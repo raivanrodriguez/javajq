5ca1f71a7e1e3b818a2631e0d4745687e799e175
raivanrodriguez <110645479+raivanrodriguez@users.noreply.github.com>
2023-09-15 01:19:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("button").click(function(){
+    $("div").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '150px',
+      width: '150px'
+    });
+  });
+});
+
+$(document).ready(function(){
+  $("p").on("click", function(){
+    $(this).hide();
+  });
+});
